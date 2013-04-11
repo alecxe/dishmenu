c932e00988a931d735905855fbaf2faa8fe23fe7
x-act <oleglysachok@gmail.com>
2013-04-11 15:29:17 +0400

    updated

--- makemenu/urls.py
+++ makemenu/urls.py
@@ -1,14 +1,12 @@
 from django.conf.urls import patterns, url
 from django.views.generic import DetailView, ListView
 from makemenu.models import DishType
+import makemenu.views
 
 urlpatterns = patterns('',
 
     url(r'^$', makemenu.views.ListDishTypeView.as_view(),
         name='dishtypes-list', ),
-    url(r'^(?P<pk>\d+)/$',
-        DetailView.as_view(
-            model=DishType,
-            template_name='makemenu/dishes.html'),
-            name='detail'),
+    url(r'^(?P<pk>\d+)/$',makemenu.views.DetailDishTypeView.as_view(),
+        name='dishes', ),
     )
--- makemenu/views.py
+++ makemenu/views.py
@@ -1,9 +1,19 @@
 # Create your views here.
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, DetailView
 from django.core.urlresolvers import reverse
-from makemenu.models import DishType
+from makemenu.models import DishType, Dish
+from django.http import HttpResponse
 
 class ListDishTypeView(ListView):
 
     model = DishType
     template_name = 'makemenu\dishtypes_list.html'
+    context_object_name='dishtypes_list'
+
+class DetailDishTypeView(DetailView):
+
+    model=DishType
+    template_name='makemenu\dishes.html'
+
+
+
